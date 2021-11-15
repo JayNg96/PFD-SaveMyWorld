57b7c2330cfa6453f94009b8fd6a15318435aa26
Jian Wei Ng <s10208269@connect.np.edu.sg>
2021-11-16 02:21:26 +0800

    Rebuild login script

--- scripts/login.js
+++ scripts/login.js
@@ -1,55 +1,39 @@
-$(document).ready(function () {
-    const APIKEY = "617ffebf63fbb2763ab02509";
+var APIKEY = "617ffebf63fbb2763ab02509";
 
-    var z = sessionStorage.getItem("loginStatus");
-    console.log(z);
+$("#invalid").hide();
+$(".loader").hide(); 
 
-    $("#submit-login").on("click", function (e) {
-        let inputEmail = $("#input-email").val();
-        let inputPassword = $("#input-password").val();
+//Login button
+$("#login-btn").on("click", function () {  
+  $(".loader").show();
+    setTimeout(function(){
+      $(".loader").fadeOut("slow"); 
+    }, 2000);  
+    let username = $("#username").val();
+    let password = $("#password").val();
 
-        checkValidity(inputEmail, inputPassword);
-
-    });
-
-    function checkValidity(inputEmail, inputPassword){
-        sessionStorage.setItem("loginStatus" , "loggedOut");
-        let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://savetheearth-c589.restdb.io/rest/registered-accounts",
-            "method": "GET",
-            "headers": {
-              "content-type": "application/json",
-              "x-apikey": APIKEY,
-              "cache-control": "no-cache"
-            },
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://savetheearth-c589.restdb.io/rest/registered-accounts",
+        "method": "GET",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": APIKEY,
+            "cache-control": "no-cache"
         }
-        $.ajax(settings).done(function (response) {
-                console.log(response);
-
-                for(var i = 0; i < response.length; i++){
-                    if(response[i].email == inputEmail){
-                        if(response[i].password == inputPassword){
-                            window.alert('Login Successful');
-                            window.location.href="../../index.html"
-                            sessionStorage.setItem("loginStatus" , "loggedIn")
-                            sessionStorage.setItem("user_id", response[i]._id);
-                            sessionStorage.setItem("full_name", response[i].full_name);
-                            sessionStorage.setItem("email", response[i].email);
-                            sessionStorage.setItem("password", response[i].password);
-                            sessionStorage.setItem("cart", response[i].cart);
-                            break;
-                        }
-                    }
-                }
-                var status = sessionStorage.getItem("loginStatus");
-                if(status == "loggedOut")
-                {
-                    window.alert('Wrong password and/or email. Please try again');
-                    document.getElementById('input-email').value = "";
-                    document.getElementById('input-password').value = "";
-                }
-        })
-    }
+    } 
+    $.ajax(settings).done(function (response) {
+        alert(response);
+        //Check if user and password match in RestDb
+        for (let j = 0; j < response.length; j++) {
+            if(username == response[j].username && password == response[j].password){
+                window.location.replace("../../index.html");
+            }
+            else
+            {
+              $("#invalid").show();
+            }
+        }
+      });
 })
