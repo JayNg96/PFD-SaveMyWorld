87a0c0652fb2a7d47abd81d47a23393753674532
Jian Wei Ng <s10208269@connect.np.edu.sg>
2021-11-16 19:34:45 +0800

    Navbar Overhaul

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-var loggedIn = sessionStorage.getItem("loginStatus");
-if (loggedIn == "loggedIn"){
-    document.querySelector("#sign-up-dropdown").removeAttribute('href');
-    var user = localStorage.getItem("full_name");
-    document.querySelector("#sign-up-dropdown").innerHTML = user;
-    document.querySelector("#login-dropdown").innerHTML = "Sign Out";
-
-    
-}
