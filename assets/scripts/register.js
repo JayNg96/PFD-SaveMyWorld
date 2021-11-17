efa616cf5fb5c9e74b69b5a230359830fe2d1d0e
toyocrabs <mljkprvt@gmail.com>
2021-11-17 16:08:27 +0800

    revert register code

--- assets/scripts/register.js
+++ assets/scripts/register.js
@@ -3,39 +3,14 @@ var APIKEY = "617ffebf63fbb2763ab02509";
 $("#registered").hide();
 $("#invalid").hide();
 $("#invalid-fill").hide();
-$("#invalid-email").hide();
 
 //Register button
 $("#register-btn").on("click", function () { 
-  let email = $("email").val();
   let username = $("#username").val();
   let password = $("#password").val();
   let c_password = $("#c-password").val();
 
-  var status = true;
-  let settings1 = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://savetheearth-c589.restdb.io/rest/registered-accounts",
-    "method": "GET",
-    "headers": {
-      "content-type": "application/json",
-      "x-apikey": APIKEY,
-      "cache-control": "no-cache"
-    },
-}
-
-$.ajax(settings).done(function (response) {
-  for(var i = 0; i < response.length; i++){
-      if(response[i].email == email){
-          $("#invalid-email").show();
-          break;
-      }
-  }
-})
-
-  function register(email, username, password, c_password){
-    //Validation check
+  //Validation check
   if(username == "" || password == "" || c_password == ""){
     $("#invalid-fill").show();
   }
@@ -50,11 +25,11 @@ $.ajax(settings).done(function (response) {
       "password":password,
       "cart":""
     };
-    
+
     let settings = {
       "async": true,
       "crossDomain": true,
-      "url": "https://savetheearth-c589.restdb.io/rest/registered-accounts",
+        "url": "https://savetheearth-c589.restdb.io/rest/registered-accounts",
       "method": "POST",
       "headers": {
         "content-type": "application/json",
@@ -64,10 +39,8 @@ $.ajax(settings).done(function (response) {
       "processData": false,
       "data": JSON.stringify(jsondata)
     }
-  
+
     $.ajax(settings).done(function (response) {
     })
   }
-  }
-  
 })
