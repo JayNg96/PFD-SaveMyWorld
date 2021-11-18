0ee3da04e74382aaeadf41bff52a04b048bdc7c4
toyocrabs <mljkprvt@gmail.com>
2021-11-18 16:42:32 +0800

    added quiz category

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -3,6 +3,7 @@
 try {
 	const start_btn = document.querySelector(".start_btn button");
 	const info_box = document.querySelector(".info_box");
+	const cat_box = document.querySelector(".cat_box");
 	const exit_btn = info_box.querySelector(".buttons .quit");
 	const continue_btn = info_box.querySelector(".buttons .restart");
 	const quiz_box = document.querySelector(".quiz_box");
@@ -22,12 +23,10 @@ try {
 		// if continueQuiz button clicked
 	continue_btn.onclick = () => {
 		info_box.classList.remove("activeInfo"); //hide info box
-		quiz_box.classList.add("activeQuiz"); //show quiz box
-		showQuetions(0); //calling showQestions function
-		queCounter(1); //passing 1 parameter to queCounter
-		startTimer(15); //calling startTimer function
-		startTimerLine(0); //calling startTimerLine function
+		cat_box.classList.add("activeCat"); //show quiz box
 	}
+	
+	let category;
 	let timeValue = 15;
 	let que_count = 0;
 	let que_numb = 1;
@@ -37,6 +36,15 @@ try {
 	let widthValue = 0;
 	const restart_quiz = result_box.querySelector(".buttons .restart");
 	const quit_quiz = result_box.querySelector(".buttons .quit");
+	function selectCat(id){
+		category = id;
+		info_box.classList.remove("activeInfo"); //hide info box
+		quiz_box.classList.add("activeQuiz"); //show quiz box
+		showQuetions(0); //calling showQestions function
+		queCounter(1); //passing 1 parameter to queCounter
+		startTimer(15); //calling startTimer function
+		startTimerLine(0); //calling startTimerLine function
+	}
 	// if restartQuiz button clicked
 	restart_quiz.onclick = () => {
 			quiz_box.classList.add("activeQuiz"); //show quiz box
@@ -63,7 +71,7 @@ try {
 	const bottom_ques_counter = document.querySelector("footer .total_que");
 	// if Next Que button clicked
 	next_btn.onclick = () => {
-			if(que_count < questions.length - 1) { //if question count is less than total question length
+			if(que_count < questions[category].length - 1) { //if question count is less than total question length
 				que_count++; //increment the que_count value
 				que_numb++; //increment the que_numb value
 				showQuetions(que_count); //calling show questions function
@@ -84,8 +92,8 @@ try {
 	function showQuetions(index) {
 		const que_text = document.querySelector(".que_text");
 		//creating a new span and div tag for question and option and passing the value using array index
-		let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
-		let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div>' + '<div class="option"><span>' + questions[index].options[1] + '</span></div>' + '<div class="option"><span>' + questions[index].options[2] + '</span></div>' + '<div class="option"><span>' + questions[index].options[3] + '</span></div>';
+		let que_tag = '<span>' + questions[category][index].numb + ". " + questions[category][index].question + '</span>';
+		let option_tag = '<div class="option"><span>' + questions[category][index].options[0] + '</span></div>' + '<div class="option"><span>' + questions[category][index].options[1] + '</span></div>' + '<div class="option"><span>' + questions[category][index].options[2] + '</span></div>' + '<div class="option"><span>' + questions[category][index].options[3] + '</span></div>';
 		que_text.innerHTML = que_tag; //adding new span tag inside que_tag
 		option_list.innerHTML = option_tag; //adding new div tag inside option_tag
 		const option = option_list.querySelectorAll(".option");
@@ -102,7 +110,7 @@ try {
 		clearInterval(counter); //clear counter
 		clearInterval(counterLine); //clear counterLine
 		let userAns = answer.textContent; //getting user selected option
-		let correcAns = questions[que_count].answer; //getting correct answer from array
+		let correcAns = questions[category][que_count].answer; //getting correct answer from array
 		const allOptions = option_list.children.length; //getting all option items
 		if(userAns == correcAns) { //if user selected option is equal to array's correct answer
 			userScore += 1; //upgrading score value with 1
@@ -135,16 +143,16 @@ try {
 		const scoreText = result_box.querySelector(".score_text");
 		if(userScore > 3) { // if user scored more than 3
 			//creating a new span tag and passing the user score number and total question number
-			let scoreTag = '<span>and congrats! ??, You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You have earned ' + userScore + ' reward points!</p>';
+			let scoreTag = '<span>and congrats! ??, You got <p>' + userScore + '</p> out of <p>' + questions[category].length + '</p></span><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You have earned ' + userScore + ' reward points!</p>';
 			scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
 			totalPoints += userScore;
 			userPoints.innerHTML = parseFloat(totalPoints.toFixed(0));
 			Storage.savePoint(totalPoints.toFixed(0));
 		} else if(userScore > 1) { // if user scored more than 1
-			let scoreTag = '<span>and nice ??, You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+			let scoreTag = '<span>and nice ??, You got <p>' + userScore + '</p> out of <p>' + questions[category].length + '</p></span>';
 			scoreText.innerHTML = scoreTag;
 		} else { // if user scored less than 1
-			let scoreTag = '<span>and sorry ??, You got only <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+			let scoreTag = '<span>and sorry ??, You got only <p>' + userScore + '</p> out of <p>' + questions[category].length + '</p></span>';
 			scoreText.innerHTML = scoreTag;
 		}
 	}
@@ -163,7 +171,7 @@ try {
 				clearInterval(counter); //clear counter
 				timeText.textContent = "Time Off"; //change the time text to time off
 				const allOptions = option_list.children.length; //getting all option items
-				let correcAns = questions[que_count].answer; //getting correct answer from array
+				let correcAns = questions[category][que_count].answer; //getting correct answer from array
 				for(i = 0; i < allOptions; i++) {
 					if(option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer
 						option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
@@ -193,38 +201,49 @@ try {
 
 	function queCounter(index) {
 		//creating a new span tag and passing the question number and total question
-		let totalQueCounTag = '<span><p>' + index + '</p> of <p>' + questions.length + '</p> Questions</span>';
+		let totalQueCounTag = '<span><p>' + index + '</p> of <p>' + questions[category].length + '</p> Questions</span>';
 		bottom_ques_counter.innerHTML = totalQueCounTag; //adding new span tag inside bottom_ques_counter
+	}
+
+	function test(id){
+		alert(id);
 	}
 } catch {
 	console.log("This page isn't the quiz page, quiz script will not be executed.")
 }
 //* ---------------------- quiz questions JS ---------------------- *//
 // creating an array and passing the number, questions, options, and answers
-let questions = [{
-	numb: 1,
-	question: "What does HTML stand for?",
-	answer: "Hyper Text Markup Language",
-	options: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"]
-}, {
-	numb: 2,
-	question: "What does CSS stand for?",
-	answer: "Cascading Style Sheet",
-	options: ["Common Style Sheet", "Colorful Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"]
-}, {
-	numb: 3,
-	question: "What does PHP stand for?",
-	answer: "Hypertext Preprocessor",
-	options: ["Hypertext Preprocessor", "Hypertext Programming", "Hypertext Preprogramming", "Hometext Preprocessor"]
-}, {
-	numb: 4,
-	question: "What does SQL stand for?",
-	answer: "Structured Query Language",
-	options: ["Stylish Question Language", "Stylesheet Query Language", "Statement Question Language", "Structured Query Language"]
-}, {
-	numb: 5,
-	question: "What does XML stand for?",
-	answer: "eXtensible Markup Language",
-	options: ["eXtensible Markup Language", "eXecutable Multiple Language", "eXTra Multi-Program Language", "eXamine Multiple Language"]
-}];
+let questions = {
+	cat1: [{
+		numb: 1,
+		question: "What does HTML stand for?",
+		answer: "Hyper Text Markup Language",
+		options: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"]
+	}, 
+	{
+		numb: 2,
+		question: "What does CSS stand for?",
+		answer: "Cascading Style Sheet",
+		options: ["Common Style Sheet", "Colorful Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"]
+	}],
+
+	cat2: [{
+		numb: 1,
+		question: "What does PHP stand for?",
+		answer: "Hypertext Preprocessor",
+		options: ["Hypertext Preprocessor", "Hypertext Programming", "Hypertext Preprogramming", "Hometext Preprocessor"]
+	}, 
+	{
+		numb: 2,
+		question: "What does SQL stand for?",
+		answer: "Structured Query Language",
+		options: ["Stylish Question Language", "Stylesheet Query Language", "Statement Question Language", "Structured Query Language"]
+	}, 
+	{
+		numb: 3,
+		question: "What does XML stand for?",
+		answer: "eXtensible Markup Language",
+		options: ["eXtensible Markup Language", "eXecutable Multiple Language", "eXTra Multi-Program Language", "eXamine Multiple Language"]
+	}]
+};
 //* ---------------------- end of quiz.html JS ---------------------- *//
