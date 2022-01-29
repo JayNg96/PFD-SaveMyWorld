8d6ae8ff98a692d8da1969d84b3f354cd9da5e00
Jian Wei Ng <s10208269@connect.np.edu.sg>
2022-01-30 00:55:41 +0800

    Add mobile view to quiz (Commit for Jun Keat)

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -13,17 +13,17 @@ try {
 	const timeCount = document.querySelector(".timer .timer_sec");
 	// if startQuiz button clicked
 	start_btn.onclick = () => {
-			info_box.classList.add("activeInfo"); //show info box
-			$("#ready-text").hide();
-		}
-		// if exitQuiz button clicked
+		info_box.classList.add("activeInfo"); //show info box
+		$("#ready-text").hide();
+	}
+	// if exitQuiz button clicked
 	exit_btn.onclick = () => {
-			info_box.classList.remove("activeInfo"); //hide info box
-			$("#ready-text").show();
-		}
-		// if continueQuiz button clicked
+		info_box.classList.remove("activeInfo"); //hide info box
+		$("#ready-text").show();
+	}
+	// if continueQuiz button clicked
 	let category;
-	function selectCat(id){
+	function selectCat(id) {
 		category = id
 		info_box.classList.remove("activeInfo"); //hide info box
 		quiz_box.classList.add("activeQuiz"); //show quiz box
@@ -41,26 +41,26 @@ try {
 	let widthValue = 0;
 	const restart_quiz = result_box.querySelector(".buttons .restart");
 	const quit_quiz = result_box.querySelector(".buttons .quit");
-	
+
 	// if restartQuiz button clicked
 	restart_quiz.onclick = () => {
-			quiz_box.classList.add("activeQuiz"); //show quiz box
-			result_box.classList.remove("activeResult"); //hide result box
-			timeValue = 15;
-			que_count = 0;
-			que_numb = 1;
-			userScore = 0;
-			widthValue = 0;
-			showQuetions(que_count); //calling showQestions function
-			queCounter(que_numb); //passing que_numb value to queCounter
-			clearInterval(counter); //clear counter
-			clearInterval(counterLine); //clear counterLine
-			startTimer(timeValue); //calling startTimer function
-			startTimerLine(widthValue); //calling startTimerLine function
-			timeText.textContent = "Time Left"; //change the text of timeText to Time Left
-			next_btn.classList.remove("show"); //hide the next button
-		}
-		// if quitQuiz button clicked
+		quiz_box.classList.add("activeQuiz"); //show quiz box
+		result_box.classList.remove("activeResult"); //hide result box
+		timeValue = 15;
+		que_count = 0;
+		que_numb = 1;
+		userScore = 0;
+		widthValue = 0;
+		showQuetions(que_count); //calling showQestions function
+		queCounter(que_numb); //passing que_numb value to queCounter
+		clearInterval(counter); //clear counter
+		clearInterval(counterLine); //clear counterLine
+		startTimer(timeValue); //calling startTimer function
+		startTimerLine(widthValue); //calling startTimerLine function
+		timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+		next_btn.classList.remove("show"); //hide the next button
+	}
+	// if quitQuiz button clicked
 	quit_quiz.onclick = () => {
 		window.location.reload(); //reload the current window
 	}
@@ -68,24 +68,24 @@ try {
 	const bottom_ques_counter = document.querySelector("footer .total_que");
 	// if Next Que button clicked
 	next_btn.onclick = () => {
-			if(que_count < questions[category].length - 1) { //if question count is less than total question length
-				que_count++; //increment the que_count value
-				que_numb++; //increment the que_numb value
-				showQuetions(que_count); //calling show questions function
-				queCounter(que_numb); //passing que_numb value to queCounter
-				clearInterval(counter); //clear counter
-				clearInterval(counterLine); //clear counterLine
-				startTimer(timeValue); //calling startTimer function
-				startTimerLine(widthValue); //calling startTimerLine function
-				timeText.textContent = "Time Left"; //change the timeText to Time Left
-				next_btn.classList.remove("show"); //hide the next button
-			} else {
-				clearInterval(counter); //clear counter
-				clearInterval(counterLine); //clear counterLine
-				showResult(); //calling showResult function
-			}
+		if (que_count < questions[category].length - 1) { //if question count is less than total question length
+			que_count++; //increment the que_count value
+			que_numb++; //increment the que_numb value
+			showQuetions(que_count); //calling show questions function
+			queCounter(que_numb); //passing que_numb value to queCounter
+			clearInterval(counter); //clear counter
+			clearInterval(counterLine); //clear counterLine
+			startTimer(timeValue); //calling startTimer function
+			startTimerLine(widthValue); //calling startTimerLine function
+			timeText.textContent = "Time Left"; //change the timeText to Time Left
+			next_btn.classList.remove("show"); //hide the next button
+		} else {
+			clearInterval(counter); //clear counter
+			clearInterval(counterLine); //clear counterLine
+			showResult(); //calling showResult function
 		}
-		// getting questions and options from array
+	}
+	// getting questions and options from array
 	function showQuetions(index) {
 		const que_text = document.querySelector(".que_text");
 		//creating a new span and div tag for question and option and passing the value using array index
@@ -95,7 +95,7 @@ try {
 		option_list.innerHTML = option_tag; //adding new div tag inside option_tag
 		const option = option_list.querySelectorAll(".option");
 		// set onclick attribute to all available options
-		for(i = 0; i < option.length; i++) {
+		for (i = 0; i < option.length; i++) {
 			option[i].setAttribute("onclick", "optionSelected(this)");
 		}
 	}
@@ -109,7 +109,7 @@ try {
 		let userAns = answer.textContent; //getting user selected option
 		let correcAns = questions[category][que_count].answer; //getting correct answer from array
 		const allOptions = option_list.children.length; //getting all option items
-		if(userAns == correcAns) { //if user selected option is equal to array's correct answer
+		if (userAns == correcAns) { //if user selected option is equal to array's correct answer
 			userScore += 1; //upgrading score value with 1
 			answer.classList.add("correct"); //adding green color to correct selected option
 			answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
@@ -119,15 +119,15 @@ try {
 			answer.classList.add("incorrect"); //adding red color to correct selected option
 			answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
 			console.log("Wrong Answer");
-			for(i = 0; i < allOptions; i++) {
-				if(option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer 
+			for (i = 0; i < allOptions; i++) {
+				if (option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer 
 					option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
 					option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
 					console.log("Auto selected correct answer.");
 				}
 			}
 		}
-		for(i = 0; i < allOptions; i++) {
+		for (i = 0; i < allOptions; i++) {
 			option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
 		}
 		next_btn.classList.add("show"); //show the next button if user selected any option
@@ -138,14 +138,14 @@ try {
 		quiz_box.classList.remove("activeQuiz"); //hide quiz box
 		result_box.classList.add("activeResult"); //show result box
 		const scoreText = result_box.querySelector(".score_text");
-		if(userScore/questions[category].length >= 0.75) { // if user scored more than 3
+		if (userScore / questions[category].length >= 0.75) { // if user scored more than 3
 			//creating a new span tag and passing the user score number and total question number
 			let scoreTag = '<span>Well done! You got <p>' + userScore + '</p> out of <p>' + questions[category].length + '</p></span>';/**<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You have earned ' + userScore + ' reward points!</p>';**/
 			scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
 			totalPoints += userScore;
 			userPoints.innerHTML = parseFloat(totalPoints.toFixed(0));
 			Storage.savePoint(totalPoints.toFixed(0));
-		} else if(userScore/questions[category].length >= 0.5) { // if user scored more than 1
+		} else if (userScore / questions[category].length >= 0.5) { // if user scored more than 1
 			let scoreTag = '<span>Nice! You got <p>' + userScore + '</p> out of <p>' + questions[category].length + '</p></span>';
 			scoreText.innerHTML = scoreTag;
 		} else { // if user scored less than 1
@@ -160,23 +160,23 @@ try {
 		function timer() {
 			timeCount.textContent = time; //changing the value of timeCount with time value
 			time--; //decrement the time value
-			if(time < 9) { //if timer is less than 9
+			if (time < 9) { //if timer is less than 9
 				let addZero = timeCount.textContent;
 				timeCount.textContent = "0" + addZero; //add a 0 before time value
 			}
-			if(time < 0) { //if timer is less than 0
+			if (time < 0) { //if timer is less than 0
 				clearInterval(counter); //clear counter
 				timeText.textContent = "Time Off"; //change the time text to time off
 				const allOptions = option_list.children.length; //getting all option items
 				let correcAns = questions[category][que_count].answer; //getting correct answer from array
-				for(i = 0; i < allOptions; i++) {
-					if(option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer
+				for (i = 0; i < allOptions; i++) {
+					if (option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer
 						option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
 						option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
 						console.log("Time Off: Auto selected correct answer.");
 					}
 				}
-				for(i = 0; i < allOptions; i++) {
+				for (i = 0; i < allOptions; i++) {
 					option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
 				}
 				next_btn.classList.add("show"); //show the next button if user selected any option
@@ -185,13 +185,28 @@ try {
 	}
 
 	function startTimerLine(time) {
-		counterLine = setInterval(timer, 29);
+		if (screen.width < 768) {
+			console.log("test1");
+			counterLine = setInterval(timer, 47);
 
-		function timer() {
-			time += 1; //upgrading time value with 1
-			time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-			if(time > 549) { //if time value is greater than 549
-				clearInterval(counterLine); //clear counterLine
+			function timer() {
+				time += 1; //upgrading time value with 1
+				time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+				if (time > 339) { //if time value is greater than 549
+					clearInterval(counterLine); //clear counterLine
+				}
+			}
+		}
+		else {
+			console.log("test2")
+			counterLine = setInterval(timer, 29);
+
+			function timer() {
+				time += 1; //upgrading time value with 1
+				time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+				if (time > 549) { //if time value is greater than 339
+					clearInterval(counterLine); //clear counterLine
+				}
 			}
 		}
 	}
@@ -212,12 +227,12 @@ let questions = {
 		question: "Which is the most air polluted country?",
 		answer: "Bangladesh",
 		options: ["Singapore", "Beijing", "Bangladesh", "USA"]
-	}, 
+	},
 	{
 		numb: 2,
 		question: "What effects of air pollution on the human body?",
 		answer: "All of the above",
-		options: ["vunerability to infections and diseases", "Damage to reproductive functions", "Increased risk of stroke and dementia", "All of the above"]
+		options: ["Vunerability to infections and diseases", "Damage to reproductive functions", "Increased risk of stroke and dementia", "All of the above"]
 	},
 	{
 		numb: 3,
@@ -229,7 +244,7 @@ let questions = {
 		numb: 4,
 		question: "What is an example of a greenhouse gas?",
 		answer: "A and B",
-		options: ["Carbon Monoxide", "Carbon Dioxide", "Oxygen", "All of the above"]
+		options: ["Carbon Monoxide", "Carbon Dioxide", "Oxygen", "A and B"]
 	},
 	{
 		numb: 5,
@@ -237,21 +252,21 @@ let questions = {
 		answer: "Ecosystem remains unchanged",
 		options: ["Climate Change", "Ozone layer gets damaged", "Ecosystem remains unchanged", "Habitats get destroyed"]
 	},
-],
+	],
 
 	deforestation: [{
 		numb: 1,
 		question: "What is the estimated annual rate of deforestation?",
 		answer: "1.3 million km2 per decade",
 		options: ["41.7 million km2 per decade", "1.3 billion km2 per decade", "1.3 million km2 per decade", "6.9 million km2 per decade"]
-	}, 
+	},
 	{
 		numb: 2,
 		question: "Which forest is considered the 'lungs' of the planet?",
 		answer: "The Amazon Rainforest",
 		options: ["The Congo Rainforest", "Daintree Rainforest", "The Amazon Rainforest", "Southeast Asian Rainforest"]
 	},
-	{  
+	{
 		numb: 3,
 		question: "Deforestation is the second leading cause of global warming in the world.",
 		answer: "True",
@@ -269,15 +284,15 @@ let questions = {
 		answer: "Plant more trees",
 		options: ["Use more paper", "Buy products containing palm oil", "Plant more trees", "All of the above"]
 	},
-],
+	],
 
 	foodWaste: [{
 		numb: 1,
 		question: "How many tons of food is wasted each year?",
 		answer: "1.3 billion tons",
 		options: ["50,000 tons", "3 billion tons", "1.3 billion tons", "100,000 tons"]
-	}, 
-	{  
+	},
+	{
 		numb: 2,
 		question: "If we produce food that doesn’t get eaten, what else is wasted?",
 		answer: "All of the above",
@@ -301,13 +316,13 @@ let questions = {
 		answer: "Donate unsold food",
 		options: ["Donate unsold food", "Throw away more food", "Order more take out", "All of the above"]
 	},
-],
+	],
 	globalWarming: [{
 		numb: 1,
 		question: "Air pollution is one of the causes of climate change",
 		answer: "True",
 		options: ["True", "False"]
-	}, 
+	},
 	{
 		numb: 2,
 		question: "What is the greenhouse effect?",
@@ -332,14 +347,14 @@ let questions = {
 		answer: "Walk, cycle or take public transport",
 		options: ["Eat less vegetables", "Throw away more food", "Walk, cycle or take public transport", "All of the above"]
 	},
-],
+	],
 
 	lossOfBiodiversity: [{
 		numb: 1,
 		question: "How does loss of biodiversity affect us?",
 		answer: "Decrease our ability to produce medicine",
 		options: ["Provides us with more food", "Protects us against natural disasters", "Decrease our ability to produce medicine", "Loss of biodiversity does not affect humans"]
-	}, 
+	},
 	{
 		numb: 2,
 		question: "Which land-based ecosystem has the most biodiversity",
@@ -364,14 +379,14 @@ let questions = {
 		answer: "Individual",
 		options: ["Species", "Genetic", "Ecosystem", "Individual"]
 	},
-],
+	],
 
 	plasticPollution: [{
 		numb: 1,
 		question: "How many million tons of plastic are dumped in our oceans every year?",
 		answer: "8 million tons",
 		options: ["1 million tons", "8 million tons", "20 million tons", "50 million tons"]
-	}, 
+	},
 	{
 		numb: 2,
 		question: "Single-use plastic accounts for ___% of the plastic produced each year",
@@ -396,6 +411,6 @@ let questions = {
 		answer: "Bring your own reusable bag",
 		options: ["Buy more plastic water bottles", "Order more takeout", "Purchase brand new items", "Bring your own reusable bag"]
 	},
-],
+	],
 };
 //* ---------------------- end of quiz.html JS ---------------------- *//
