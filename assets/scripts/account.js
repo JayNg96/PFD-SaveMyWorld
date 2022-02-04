24b51efd4c8902b862885207245f379e3737b39e
Jian Wei Ng <s10208269@connect.np.edu.sg>
2022-02-04 19:41:29 +0800

    Site optimization - minify js files

--- assets/scripts/account.js
+++ assets/scripts/account.js
@@ -1,25 +1,2 @@
-var APIKEY = "617ffebf63fbb2763ab02509";
-
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://savetheearth-c589.restdb.io/rest/event-signup",
-    "method": "GET",
-    "headers": {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache"
-    }
-}
-$.ajax(settings).done(function (response) {
-    //Check if user and password match in RestDb
-    for (let j = 0; j < response.length; j++) {
-        if (username == response[j].username && password == response[j].password) {
-            sessionStorage.setItem("loginStatus", "loggedIn");
-            sessionStorage.setItem("username", response[j].username);
-            window.location.replace("../../index.html");
-        } else {
-            $("#invalid").show();
-        }
-    }
-});
+var APIKEY = "617ffebf63fbb2763ab02509"; var settings = { "async": !0, "crossDomain": !0, "url": "https://savetheearth-c589.restdb.io/rest/event-signup", "method": "GET", "headers": { "content-type": "application/json", "x-apikey": APIKEY, "cache-control": "no-cache" } }
+$.ajax(settings).done(function (response) { for (let j = 0; j < response.length; j++) { if (username == response[j].username && password == response[j].password) { sessionStorage.setItem("loginStatus", "loggedIn"); sessionStorage.setItem("username", response[j].username); window.location.replace("../../index.html") } else { $("#invalid").show() } } })
